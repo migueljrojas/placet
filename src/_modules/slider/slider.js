e6a128636d434bc56fdb5d3db696ca525ce80e1f
Your Name <you@example.com>
2018-12-27 12:30:37 -0300

    error corrections

--- src/_modules/slider/slider.js
+++ src/_modules/slider/slider.js
@@ -9,7 +9,7 @@ var Slider = function() {
             $(this).slick({
                 dots: true,
                 fade: true,
-                arrows: true,
+                arrows: false,
                 swipeToSlide: true,
                 autoplay: true
             });
